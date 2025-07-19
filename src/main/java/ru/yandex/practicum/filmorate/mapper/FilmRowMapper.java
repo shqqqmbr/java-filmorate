d8f031cc41c479b64ff4e3c0a4a087832458e720
berbenets_m <berbenets.maksim@bk.ru>
2025-07-19 14:20:34 +0300

    Исправлены Film/Review мапперы

--- src/main/java/ru/yandex/practicum/filmorate/mapper/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/FilmRowMapper.java
@@ -1,22 +1,21 @@
 package ru.yandex.practicum.filmorate.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.loader.FilmDataLoader;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
 
 public class FilmRowMapper implements RowMapper<Film> {
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final FilmDataLoader dataLoader;
 
     public FilmRowMapper(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.dataLoader = new FilmDataLoader(namedParameterJdbcTemplate);
     }
 
     @Override
@@ -33,52 +32,8 @@ public class FilmRowMapper implements RowMapper<Film> {
         mpa.setName(resultSet.getString("mpa_name"));
         film.setMpa(mpa);
 
-        Set<Genre> genres = getGenresForFilm(film.getId());
-        film.setGenres(genres);
-
-        Set<Integer> likes = getLikesForFilm(film.getId());
-        film.setLikes(likes);
-
-        Set<Director> directors = getDirectorsForFilm(film.getId());
-        film.setDirectors(directors);
+        dataLoader.loadFilmAdditionalData(film);
 
         return film;
-    }
-
-    private Set<Genre> getGenresForFilm(long filmId) {
-        String sql = """
-                SELECT g.genre_id, g.genre_name
-                FROM genres g
-                    JOIN film_genres fg ON g.genre_id = fg.genre_id
-                WHERE fg.film_id = :filmId
-                ORDER BY g.genre_id ASC
-                """;
-        return new LinkedHashSet<>(namedParameterJdbcTemplate.query(
-                sql,
-                new MapSqlParameterSource("filmId", filmId),
-                new GenreRowMapper()
-        ));
-    }
-
-    private Set<Integer> getLikesForFilm(int filmId) {
-        String sql = """
-                SELECT user_id
-                FROM likes
-                WHERE film_id = :filmId
-                """;
-        return new HashSet<>(namedParameterJdbcTemplate.queryForList(
-                sql,
-                new MapSqlParameterSource("filmId", filmId),
-                Integer.class
-        ));
-    }
-
-    private Set<Director> getDirectorsForFilm(int filmId) {
-        String sql = "SELECT d.* FROM directors d " +
-                "JOIN film_directors fd ON d.director_id = fd.director_id " +
-                "WHERE fd.film_id = :filmId";
-
-        Map<String, Object> params = Collections.singletonMap("filmId", filmId);
-        return new LinkedHashSet<>(namedParameterJdbcTemplate.query(sql, params, new DirectorRowMapper()));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/mapper/ReviewRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/ReviewRowMapper.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.mapper;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import ru.yandex.practicum.filmorate.loader.ReviewDataLoader;
 import ru.yandex.practicum.filmorate.model.Review;
 
 import java.sql.ResultSet;
@@ -9,9 +10,11 @@ import java.sql.SQLException;
 
 public class ReviewRowMapper implements RowMapper<Review> {
     JdbcTemplate jdbcTemplate;
+    private final ReviewDataLoader dataLoader;
 
     public ReviewRowMapper(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.dataLoader = new ReviewDataLoader(jdbcTemplate);
     }
 
     @Override
@@ -22,13 +25,7 @@ public class ReviewRowMapper implements RowMapper<Review> {
         review.setIsPositive(resultSet.getBoolean("is_positive"));
         review.setUserId(resultSet.getInt("user_id"));
         review.setFilmId(resultSet.getInt("film_id"));
-        review.setUseful(calculateUseful(review.getReviewId()));
+        review.setUseful(dataLoader.calculateUseful(review.getReviewId()));
         return review;
-    }
-
-    private int calculateUseful(int reviewId) {
-        String sql = "SELECT SUM(useful) FROM review_likes WHERE review_id = ?";
-        Integer rating = jdbcTemplate.queryForObject(sql, Integer.class, reviewId);
-        return rating != null ? rating : 0;
     }
 }
